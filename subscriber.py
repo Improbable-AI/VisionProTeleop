43cdfad794716787aa89b2d9a0bd260a97153af6
younghyo <younghyo@mit.edu>
2024-02-13 16:21:12 -0500

    fix grpc bug

--- subscriber.py
+++ subscriber.py
@@ -1,21 +1,29 @@
-from concurrent import futures
+# subscriber.py
+
+import os
+import logging
+
+# os.environ['GRPC_VERBOSITY'] = 'DEBUG'
+# # os.environ['GRPC_TRACE'] = 'all'
+# logging.basicConfig(level=logging.DEBUG)
+
+
 import grpc
-import hand_pb2
-import hand_pb2_grpc
+import handtracking_pb2
+import handtracking_pb2_grpc
 
-class HandTrackingService(hand_pb2_grpc.HandTrackingServiceServicer):
-    def StreamHandUpdates(self, request_iterator, context):
-        for hand_update in request_iterator:
-            print(f"Received update for left hand: {hand_update.left_hand}")
-            print(f"Received update for right hand: {hand_update.right_hand}")
-            yield handtracking_pb2.HandUpdateAck(message="Update Received")
+def run():
 
-def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    hand_pb2_grpc.add_HandTrackingServiceServicer_to_server(HandTrackingService(), server)
-    server.add_insecure_port('[::]:50051')
-    server.start()
-    server.wait_for_termination()
+    request = handtracking_pb2.HandUpdate()
+
+    with grpc.insecure_channel('localhost:12345') as channel:
+        stub = handtracking_pb2_grpc.HandTrackingServiceStub(channel)
+        # Correctly invoking a server-streaming RPC
+        responses = stub.StreamHandUpdates(request)
+        for response in responses:
+            print(f"Received update for left hand: {response.left_hand}")
+            print(f"Received update for right hand: {response.right_hand}")
 
 if __name__ == '__main__':
-    serve()
+    print("Starting the gRPC client")
+    run()
