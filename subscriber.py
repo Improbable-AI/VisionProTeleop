0f134821cb80f4a25d6c3abb5499d1688d73760b
younghyo <younghyo@mit.edu>
2024-02-15 17:10:51 -0500

    add HeadTracking + matrix handler

--- subscriber.py
+++ subscriber.py
@@ -3,14 +3,10 @@
 import os
 import logging
 
-# os.environ['GRPC_VERBOSITY'] = 'DEBUG'
-# # os.environ['GRPC_TRACE'] = 'all'
-# logging.basicConfig(level=logging.DEBUG)
-
-
 import grpc
 import handtracking_pb2
 import handtracking_pb2_grpc
+import numpy as np 
 
 import argparse
 
@@ -23,9 +19,17 @@ def run(args):
         # Correctly invoking a server-streaming RPC
         responses = stub.StreamHandUpdates(request)
         for response in responses:
-            print(f"Received update for left hand: {response.left_hand.wristMatrix}")
-            print(f"Received update for right hand: {response.right_hand.wristMatrix}")
-            print(f"Received update for head: {response.Head}")
+            print(f"Received update for left hand: \n {process_matrix(response.left_hand.wristMatrix)}")
+            print(f"Received update for right hand: \n {process_matrix(response.right_hand.wristMatrix)}")
+            print(f"Received update for head: \n {process_matrix(response.Head)}")
+            print('\n')
+
+
+def process_matrix(message): 
+    return np.array([[message.m00, message.m01, message.m02, message.m03],
+                     [message.m10, message.m11, message.m12, message.m13],
+                     [message.m20, message.m21, message.m22, message.m23],
+                     [message.m30, message.m31, message.m32, message.m33]])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
