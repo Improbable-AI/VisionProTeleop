d8006806e8392334cd1dd8dff463939b8f266992
younghyopark <younghyo@mit.edu>
2025-11-25 14:20:37 -0500

    pypi version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='avp_stream',
-    version='2.2',
+    version='2.4',
     description='This python package streams diverse tracking data available from AVP to any devices that can communicate with gRPC.',
     author='Younghyo Park',
     author_email='younghyo@mit.edu',
